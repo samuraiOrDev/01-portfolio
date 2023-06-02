2c6bf88fab4e247c0e732c892948dac52a61eda8
samuraiOrDev <vm.ordiales.garcia@gmail.com>
2023-06-02 19:54:19 +0200

    Change Format HeroBanner  - 19:54 - 2-06-2023

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,6 +1,6 @@
 export const frontMatter = {
-  title: "WebPersonal | Samurai.Dev.Ord",
+  title: "WebPersonal | SamuraiOr.Dev",
   description:
-    "WebPersonal del desarrollador web FullStack Samurai.Dev.Ord. En ella podrás encontrar información sobre mi, mis proyectos y mis redes sociales. ¡Bienvenido!",
+    "WebPersonal del desarrollador web FullStack SamuraiOr.Dev. En ella podrás encontrar información sobre mi, mis proyectos y mis redes sociales. ¡Bienvenido!",
   author: "Víctor Manuel Ordiales García",
 };
