5e9b94986f7765ad54fa707012248881fe33036f
samuraiOrDev <vm.ordiales.garcia@gmail.com>
2023-06-04 19:56:30 +0200

    End SendEmail/Form  - 19:56 - 4-06-2023

--- src/components/ContactSection/FormContact.tsx
+++ src/components/ContactSection/FormContact.tsx
@@ -1,41 +1,50 @@
 import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { Input } from "./Input";
+type FormContactProps = {
+  name: string;
+  email: string;
+  message: string;
+  subject: string;
+};
 export const FormContact = () => {
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    console.log(e.target)
-  }
+  const initialValues = {
+    name: "",
+    email: "",
+    message: "",
+    subject: "",
+  };
+  const validationSchema = Yup.object({
+    name: Yup.string().required("El nombre es requerido"),
+    email: Yup.string()
+      .email("Ingrese un correo electrónico válido")
+      .required("El correo electrónico es requerido"),
+    message: Yup.string().required("El mensaje es requerido"),
+    subject: Yup.string().required("El asunto es requerido"),
+  });
+  const handleSubmit = async (values: FormContactProps) => {
+    const { name, email, message, subject } = values;
+    const data = await fetch('/api/sendEmail',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({name, email, message, subject})
+    })
+    const response = await data.json()
+    console.log(response)
+  };
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: handleSubmit,
+  });
   return (
-    <form className="mt-6 w-full md:w-1/2" onSubmit={handleSubmit}>
-      <div className="mb-6">
-        <label
-          htmlFor="name"
-          className="block mb-2 text-sm font-medium text-white"
-        >
-          Nombre:
-        </label>
-        <input
-          type="text"
-          id="name"
-          className="border text-sm rounded-lg focus:ring-yellow-500 focus:border-yellow-500 block w-full p-2.5 bg-gray-950 border-yellow-600 placeholder-yellow-600 text-white"
-          placeholder="Su nombre"
-          required
-        />
-      </div>
-      <div className="mb-6">
-        <label
-          htmlFor="email"
-          className="block mb-2 text-sm font-medium text-white"
-        >
-          Email:
-        </label>
-        <input
-          type="email"
-          id="email"
-          className="border text-sm rounded-lg focus:ring-yellow-500 focus:border-yellow-500 block w-full p-2.5 bg-gray-950 border-yellow-600 placeholder-yellow-600 text-white"
-          required
-          placeholder="example@gmail.com"
-        />
-      </div>
+    <form className="mt-6 w-full md:w-1/2" onSubmit={formik.handleSubmit}>
+      <Input id="name" type="text" title="Nombre:" formik={formik} />
+      <Input id="email" type="email" title="Correo electrónico:" formik={formik} />
+      <Input id="subject" type="text" title="Asunto:" formik={formik} />
       <div className="mb-6">
         <label
           htmlFor="message"
@@ -46,9 +55,16 @@ export const FormContact = () => {
         <textarea
           id="message"
           rows={4}
+          name="message"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.message}
           className="block p-2.5 w-full text-sm rounded-lg border bg-gray-950 border-yellow-600 placeholder-yellow-600 text-white focus:ring-yellow-600 focus:border-yellow-500"
           placeholder="Deje su comentario..."
         ></textarea>
+        {formik.touched.message && formik.errors.message ? (
+          <div className="text-[14px] mt-1 text-red-600">{formik.errors.message}</div>
+        ) : null}
       </div>
       <button type="submit" className="text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center bg-yellow-600">
         Enviar
---
+++ src/pages/api/sendEmail.tsx
@@ -0,0 +1,120 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import nodemailer from "nodemailer";
+const HTML_TEMPLATE = (message: string, email: string, name: string) => {
+  return `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <meta charset="utf-8">
+          <title>NodeMailer Email Template</title>
+          <style>
+            .container {
+              width: 100%;
+              height: 100%;
+              padding: 20px;
+              background-color: #f4f4f4;
+            }
+            .email {
+              width: 80%;
+              margin: 0 auto;
+              background-color: #fff;
+              padding: 20px;
+            }
+            .email-header {
+              background-color: #333;
+              color: #fff;
+              padding: 20px;
+              text-align: center;
+            }
+            .email-body {
+              padding: 20px;
+            }
+            .email-footer {
+              background-color: #333;
+              color: #fff;
+              padding: 20px;
+              text-align: center;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="container">
+            <div class="email">
+              <ul>
+                <li>Nombre: ${name}</li>
+                <li>Email: ${email}</li>
+              </ul>
+              <div>
+                ${message}
+              </div>
+            </div>
+          </div>
+        </body>
+      </html>
+    `;
+};
+
+type TypeData = {
+  name: string;
+  email: string;
+  message: string;
+  subject: string;
+};
+
+const sendMail = async ({ name, email, message, subject }: TypeData) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    // secure: false,
+    auth: {
+      user: process.env.GMAIL_USER, //Change to your email address
+      pass: process.env.GMAIL_PASSWORD_APP, //Change to your password
+    },
+  });
+
+  transporter.verify((error, success) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Server is ready to take messages");
+    }
+  });
+  const options = {
+    from: "Web personal samuraior.dev <samurai.dev.ordiales@gmail.com>",
+    to: process.env.GMAIL_USER, //Change to email address that you want to receive messages on
+    subject: subject,
+    html: HTML_TEMPLATE(message, email, name),
+  };
+  try {
+    // Envía el correo electrónico
+    const info = await transporter.sendMail(options);
+    console.log("Correo electrónico enviado:", info.response);
+    return { success: true };
+  } catch (error) {
+    console.error("Error al enviar el correo electrónico:", error);
+    return { success: false };
+  }
+};
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
+    const { name, email, message, subject } = req.body;
+    if (!name || !email || !message || !subject)
+      return res.status(400).json({ message: "Please fill all fields" });
+    const result = await sendMail({ name, email, message, subject });
+    if (result.success) {
+      return res
+        .status(200)
+        .json({ message: "Correo electrónico enviado", name, email, subject });
+    } else {
+      return res
+        .status(500)
+        .json({ message: "Error al enviar el correo electrónico" });
+    }
+  } else {
+    res.status(405).json({ message: "Solo se permiten solicitudes POST<" });
+  }
+};
+
+export default handler;
