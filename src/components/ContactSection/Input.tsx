da21baaf81bee8167b62808a768e0b23eed5b171
samuraiOrDev <vm.ordiales.garcia@gmail.com>
2023-06-05 15:54:43 +0200

    End SendEmail/Form - PlaceHolder  - Variables de Entorno - 19:56 - 4-06-2023

--- src/components/ContactSection/Input.tsx
+++ src/components/ContactSection/Input.tsx
@@ -20,7 +20,7 @@ export const Input: FC<Props> = ({ id, type, title, formik }) => {
         onBlur={formik.handleBlur}
         value={formik.values[`${id}`]}
         className="border text-sm rounded-lg focus:ring-yellow-500 focus:border-yellow-500 block w-full p-2.5 bg-gray-950 border-yellow-600 placeholder-yellow-600 text-white"
-        placeholder="Su nombre"
+        placeholder={title}
       />
       {formik.touched[`${id}`] && formik.errors[`${id}`] ? (
         <div className="text-[14px] mt-1 text-red-600">
