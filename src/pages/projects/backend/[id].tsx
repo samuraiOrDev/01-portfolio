cc6252067c721eb9740548e7f84a8e6f8474bbde
samuraiOrDev <vm.ordiales.garcia@gmail.com>
2023-07-10 22:42:53 +0200

    End Commit  - 22:42 - 10-07-2023

--- src/pages/projects/backend/[id].tsx
+++ src/pages/projects/backend/[id].tsx
@@ -1,4 +1,4 @@
-import { Footer, Layout, NavBar } from '@/components'
+import { Footer, ProjectsLayouts, NavBar } from '@/components'
 import React from 'react'
 import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
@@ -8,11 +8,11 @@ const itemsListNavBar = [
 ];
 const Project = () => {
   return (
-    <Layout  title={'Project | Víctor Manuel'} description={''} font={inter.className} >
+    <ProjectsLayouts  title={'Project | Víctor Manuel'} description={''} font={inter.className} >
         <NavBar itemsListNavBar={itemsListNavBar}/>
         <h1>Project | Víctor Manuel Ordiales García</h1>
         <Footer />
-    </Layout>
+    </ProjectsLayouts>
   )
 }
 
