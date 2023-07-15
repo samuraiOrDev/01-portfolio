f81ec31d3be7f86f363f2987f113288456e72137
samuraiOrDev <vm.ordiales.garcia@gmail.com>
2023-07-15 12:57:49 +0200

    AcitveClassName Link NavBar Off - 12:57 - 15-07-2023

--- src/components/NavBar/ItemsNavBar/Item.tsx
+++ src/components/NavBar/ItemsNavBar/Item.tsx
@@ -1,21 +1,14 @@
-import { ContextAsPath } from "@/context/ProviderAsPath";
 import Link from "next/link";
 import React, { FC } from "react";
-import { useContext } from "react";
 interface Props {
   title: string;
   to: string;
 }
 
 export const Item: FC<Props> = ({ title, to }) => {
-  const { asPathURL } = useContext(ContextAsPath);
-  const activesClassName =
-    asPathURL === to
-      ? "cursor-pointer text-yellow-600"
-      : "cursor-pointer";
   return (
     <div className="flex justify-center items-center  last:mr-0 font-bold hover:text-yellow-600 transition ease-out duration-300">
-      <Link href={to} className={activesClassName} title={title}>
+      <Link href={to} className={'cursor-pointer'} title={title}>
         {title}
       </Link>
     </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,7 @@ import "@/styles/HeroSection.css";
 import "@/styles/SkillSectionTabs.css";
 import "@/styles/SliderReact.css";
 import type { AppProps } from "next/app";
-import ProviderAsPath from "@/context/ProviderAsPath";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <ProviderAsPath>
-      <Component {...pageProps} />{" "}
-    </ProviderAsPath>
-  );
+  return <Component {...pageProps} />;
 }
