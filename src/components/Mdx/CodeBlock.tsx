ca797d79772eea60f9abdd07c6945056f9526e9b
samuraiOrDev <vm.ordiales.garcia@gmail.com>
2023-08-15 22:25:01 +0200

    End Theme Code - 22:18 - 15-08-2023

--- src/components/Mdx/CodeBlock.tsx
+++ src/components/Mdx/CodeBlock.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { oneDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { synthwave84} from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 interface Props {
   text: string;
@@ -11,7 +11,7 @@ export const CodeBlock: FC<Props> = ({ text, className = "java" }) => {
   console.log(language);
   return (
     <div className="mt-10 mb-10 w-[auto]">
-      <SyntaxHighlighter language={'js'} style={oneDark} wrapLongLines className=" text-[10px]  sm:text-[12px] md:text-[16px]">
+      <SyntaxHighlighter language={'java'} style={synthwave84} wrapLongLines className=" text-[10px]  sm:text-[12px] md:text-[16px]">
         {text}
       </SyntaxHighlighter>
     </div>
