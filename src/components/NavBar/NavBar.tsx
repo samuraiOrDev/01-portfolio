21831f361e50ba28ca36066913f3140e6c73019a
samuraiOrDev <vm.ordiales.garcia@gmail.com>
2023-08-15 21:14:41 +0200

    Link Logo - 21:15 - 15-08-2023

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,6 +2,7 @@ import { FC, useContext, useEffect } from "react";
 //Components
 import { LogoPrincipal } from "./Logo";
 import { ItemsNavBar, TypeItemsListNavBar } from "./ItemsNavBar";
+import Link from "next/link";
 interface Props {
   itemsListNavBar: TypeItemsListNavBar[];
 }
@@ -21,7 +22,9 @@ export const NavBar: FC<Props> = ({ itemsListNavBar }) => {
   return (
     <nav className="fixed w-full z-[6] p-6 flex justify-center items-center bg-gray-950">
       <div className="flex justify-between items-center w-full xl:max-w-[1280px] lg:flex-row flex-col">
-        <LogoPrincipal />
+        <Link href={"/"}>
+          <LogoPrincipal />
+        </Link>
         <ItemsNavBar itemsListNavBar={itemsListNavBar} />
       </div>
     </nav>
